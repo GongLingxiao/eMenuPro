b13bdb4f496de4b287d0b843b7329798fc94db67
Gong Lingxiao <zziria@163.com>
2014-02-08 18:01:37 +0800

    1
    1

---
+++ eMenuPro/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  eMenuPro
+//
+//  Created by Gong Lingxiao on 14-2-8.
+//  Copyright (c) 2014年 Gong Lingxiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
